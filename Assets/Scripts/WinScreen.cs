fe7f8ecf47326202f8f1552eb3b41e4674ea5722
taggatips <nathy.j@gmx.net>
2023-04-18 21:00:06 +0200

    add menu and win screen

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
