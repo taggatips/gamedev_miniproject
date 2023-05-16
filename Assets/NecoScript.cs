eb6a9d10e598cde2b8fbdeb701cd30a6733eecec
ProCarpet <18231509+ProCarpet@users.noreply.github.com>
2023-05-16 16:55:23 +0200

    finished rough tutorial

--- Assets/NecoScript.cs
+++ Assets/NecoScript.cs
@@ -6,22 +6,38 @@ public class NecoScript : MonoBehaviour
 {
     [Header("Game Ojects")]
     private Animator animator;
+    public Vector3 targetPosition;
     private bool hasSquished = false; 
     private int counter = 0; 
+    private Vector3 stepDistance; 
+    private Vector3 startPosition; 
+    private bool onStart= true; 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        startPosition = transform.position; 
+
+        stepDistance = targetPosition - startPosition; 
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Conductor.instance.onBeat() && !hasSquished){
-            if((counter % 4) == 0){
+            if(counter >= 4){
                 counter = 0;
-                //transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
-
+                if(onStart){
+                    transform.position = transform.position + stepDistance;
+                    transform.Rotate(0f,180f,0f);
+                    onStart = false; 
+                }
+                else{
+                    transform.position = transform.position - stepDistance;
+                    transform.Rotate(0f,180f,0f);
+                    onStart = true; 
+                }
+                
             }else{
             counter++;
             animator.SetTrigger("NecoSquish");
@@ -33,4 +49,5 @@ public class NecoScript : MonoBehaviour
         }
         
     }
+    
 }
